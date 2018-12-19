9d6ed030ad3239f5bfdb377d72c7007634e88c28
Yu Jing <yuikns@users.noreply.github.com>
2018-12-19 14:39:22 -0500

    build from source

--- Formula/kompose.rb
+++ Formula/kompose.rb
@@ -1,18 +1,22 @@
 # It was maintained in https://github.com/Homebrew/homebrew-core/blob/master/Formula/kompose.rb
 # However, this version will only download the release from official repo
+# See: https://git.io/fpjD5
 class Kompose < Formula
   desc "Go from Docker Compose to Kubernetes"
   homepage "https://github.com/kubernetes/kompose"
 
 
-  url "https://github.com/kubernetes/kompose/releases/download/v1.17.0/kompose-darwin-amd64.tar.gz"
-  # mirror "https://xxx/v1.17.0/kompose-darwin-amd64.tar.gz"
-  sha256 "4e409aeedea1be57e2f6613586d1b04aa2d97e9bceaea7cd1e36e744c54bafdc"
+  url "https://github.com/kubernetes/kompose/archive/v1.17.0.zip"
+  # mirror "https://xxx/v1.17.0/v1.17.0.zip"
+  sha256 "1c9b0125aee67aeaadd5817ee3e2ea2b4f1f8d5d8210b9f55bcb9d0814b089cc"
 
+  depends_on "go" => :build
 
   def install
-    # mkdir_p buildpath/"bin"
-    ln_s buildpath/"kompose-darwin-amd64", buildpath/"kompose"
+    ENV["GOPATH"] = buildpath
+    mkdir_p buildpath/"src/github.com/kubernetes"
+    ln_s buildpath, buildpath/"src/github.com/kubernetes/kompose"
+    system "make", "bin"
     bin.install "kompose"
 
     output = Utils.popen_read("#{bin}/kompose completion bash")
